19cbc236dd177d6e40978b3578c54c77932d7387
ImLolloTv <lorenig08@gmail.com>
2025-11-01 14:03:19 +0100

    Display dynamic Discord user info and avatar decoration
    Fetches Discord user data from the Lanyard API and updates the page with the user's avatar, avatar decoration, global name, and username. Adds new styles and elements for avatar decoration and improves main section animation and layout.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-// https://api.lanyard.rest/v1/users/623496189056253953
+const $ = (sel, root = document) => root.querySelector(sel);
+
+const userId = "623496189056253953";
+
+window.onload = async function() {
+    const response = await fetch(`https://api.lanyard.rest/v1/users/${userId}`);
+
+    if (!response.ok) {
+        console.log(response)
+    } else {
+        const body = JSON.parse(await response.text());
+
+        let userData = body.data.discord_user;
+        let discordId = userData.id;
+
+        let immagine, avatarDecoration;
+
+        if (userData.avatar && userData.avatar.startsWith("a_")) {
+            immagine = `https://cdn.discordapp.com/avatars/${discordId}/${userData.avatar}.gif`;
+        } else if (userData.avatar) {
+            immagine = `https://cdn.discordapp.com/avatars/${discordId}/${userData.avatar}.png`;
+        } else {
+            if (userData.discriminator === 0 || userData.discriminator === "0") {
+                immagine = `https://cdn.discordapp.com/embed/avatars/${((discordId >> 22) % 6)}.png`;
+            } else {
+                immagine = `https://cdn.discordapp.com/embed/avatars/${(userData.discriminator % 5)}.png`;
+            }
+        };
+
+        if (userData?.avatar_decoration_data?.asset) {
+            avatarDecoration = `https://cdn.discordapp.com/avatar-decoration-presets/${userData?.avatar_decoration_data?.asset}.png`
+        }
+
+        $(".logo").src = immagine;
+        $(".avatarDecoration").src = avatarDecoration;
+
+        $("h1").innerHTML = userData.global_name;
+        $("h4").innerHTML = userData.username;
+
+        document.title = `${userData.global_name} (@${userData.username})`
+
+        $("main").style.opacity = "1";
+        $("main").style.marginTop = "unset";
+    }
+}
